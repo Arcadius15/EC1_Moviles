a6bbd0776a9ba452b240e7a50ab3a1cd1040ff2d
R-SIGMA <84481623+R-SIGMA@users.noreply.github.com>
2022-05-10 19:02:17 -0500

    ejercicio 1

---
+++ app/src/main/java/edu/pe/idat/ec1_moviles/Ejercicio1Activity.kt
@@ -0,0 +1,45 @@
+package edu.pe.idat.ec1_moviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import edu.pe.idat.ec1_moviles.databinding.ActivityEjercicio1Binding
+
+class Ejercicio1Activity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var  binding: ActivityEjercicio1Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEjercicio1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalcDscto.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?){
+        var precio = 11000
+        var nrocomps = binding.etNroComp.text.toString().toInt()
+
+        if (nrocomps == 0){
+            binding.tvDsctoResult.text = "Debe ingresar al menos una computadora!"
+            return
+        }
+
+        var pretotal = precio * nrocomps
+
+        var dsctoporcentaje = if (nrocomps < 5){
+            10.0
+        } else if (nrocomps in 5..9){
+            20.0
+        } else {
+            40.0
+        }
+
+        var dscto = (dsctoporcentaje / 100.0) * pretotal
+        var total = pretotal - dscto
+
+        binding.tvDsctoResult.text = "Pre-total: $$pretotal \n" +
+                "Descuento: $${dscto.toInt()} ($dsctoporcentaje%) \n" +
+                "Total: $${total.toInt()}"
+    }
+}
