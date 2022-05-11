12e81fb4f5010f22357f4974e4c214e4c333e4fa
R-SIGMA <84481623+R-SIGMA@users.noreply.github.com>
2022-05-10 19:54:07 -0500

    intent de primer ejercicio

--- app/src/main/java/edu/pe/idat/ec1_moviles/Ejercicio1Activity.kt
+++ app/src/main/java/edu/pe/idat/ec1_moviles/Ejercicio1Activity.kt
@@ -1,5 +1,6 @@
 package edu.pe.idat.ec1_moviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,32 +15,37 @@ class Ejercicio1Activity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         binding.btnCalcDscto.setOnClickListener(this)
+        binding.btnVolver.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?){
-        var precio = 11000
-        var nrocomps = binding.etNroComp.text.toString().toInt()
+    override fun onClick(v: View){
+        if (v.id == R.id.btnCalcDscto){
+            var precio = 11000
+            var nrocomps = binding.etNroComp.text.toString().toInt()
 
-        if (nrocomps == 0){
-            binding.tvDsctoResult.text = "Debe ingresar al menos una computadora!"
-            return
+            if (nrocomps == 0){
+                binding.tvDsctoResult.text = "Debe ingresar al menos una computadora!"
+                return
+            }
+
+            var pretotal = precio * nrocomps
+
+            var dsctoporcentaje = if (nrocomps < 5){
+                10.0
+            } else if (nrocomps in 5..9){
+                20.0
+            } else {
+                40.0
+            }
+
+            var dscto = (dsctoporcentaje / 100.0) * pretotal
+            var total = pretotal - dscto
+
+            binding.tvDsctoResult.text = "Pre-total: $$pretotal \n" +
+                    "Descuento: $${dscto.toInt()} ($dsctoporcentaje%) \n" +
+                    "Total: $${total.toInt()}"
+        } else if (v.id == R.id.btnVolver){
+            startActivity(Intent(this@Ejercicio1Activity, MainActivity::class.java))
         }
-
-        var pretotal = precio * nrocomps
-
-        var dsctoporcentaje = if (nrocomps < 5){
-            10.0
-        } else if (nrocomps in 5..9){
-            20.0
-        } else {
-            40.0
-        }
-
-        var dscto = (dsctoporcentaje / 100.0) * pretotal
-        var total = pretotal - dscto
-
-        binding.tvDsctoResult.text = "Pre-total: $$pretotal \n" +
-                "Descuento: $${dscto.toInt()} ($dsctoporcentaje%) \n" +
-                "Total: $${total.toInt()}"
     }
 }
