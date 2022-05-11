b602f36b2ce357fe192dc06995bce3c8ff52cfff
Diego Vicente <diegovic99@hotmail.com>
2022-05-10 21:19:07 -0500

    falta el binding

---
+++ app/src/main/java/edu/pe/idat/ec1_moviles/Ejercicio3Activity.kt
@@ -0,0 +1,11 @@
+package edu.pe.idat.ec1_moviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio3)
+    }
+}
